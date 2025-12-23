23b6f5f3933aca152dc46bc445082e4c8efcbd5f
chash-code <panchalchash@gmail.com>
2025-12-23 23:36:38 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Todos from './Todos';
+
+function App() {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      backgroundColor: '#e8f5e9',
+      padding: '40px 20px'
+    }}>
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
